fd0720acd32899ea0f4bb786f168d39bfe4714e9
Paul Reioux <reioux@gmail.com>
2013-11-25 00:48:14 -0600

    Asynchronous Fsync: initial extraction of Async Fsync from HTC
    Signed-off-by: Paul Reioux <reioux@gmail.com>
    
    Conflicts:
    
    	drivers/mmc/core/mmc.c
    
    Conflicts:
    	fs/Kconfig
    	include/linux/fs.h
    
    Signed-off-by: Ryan Andri <ryan.apx07@gmail.com>

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -1511,6 +1511,11 @@ struct super_block {
 	/* Being remounted read-only */
 	int s_readonly_remount;
 
+#ifdef CONFIG_ASYNC_FSYNC
+#define FLAG_ASYNC_FSYNC        0x1
+	unsigned int fsync_flags;
+#endif
+
 	/*
 	 * Indicates how deep in a filesystem stack this SB is
 	 */
