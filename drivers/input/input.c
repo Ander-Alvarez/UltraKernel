5c02c7b997aebb83763371d3d2b56a1f152212de
Eric Farias <ericfari15@gmail.com>
2015-08-28 18:20:01 -0300

    DT2W for condor (1/2)

--- drivers/input/input.c
+++ drivers/input/input.c
@@ -37,6 +37,14 @@ MODULE_LICENSE("GPL");
 static LIST_HEAD(input_dev_list);
 static LIST_HEAD(input_handler_list);
 
+#ifdef CONFIG_TOUCHSCREEN_PREVENT_SLEEP
+#ifdef CONFIG_TOUCHSCREEN_DOUBLETAP2WAKE
+#include <linux/input/doubletap2wake.h>
+extern bool screen_suspended;
+extern bool prox_covered;
+#endif
+#endif
+
 /*
  * input_mutex protects access to both input_dev_list and input_handler_list.
  * This also causes input_[un]register_device and input_[un]register_handler
@@ -350,6 +358,12 @@ void input_event(struct input_dev *dev,
 		 unsigned int type, unsigned int code, int value)
 {
 	unsigned long flags;
+
+
+        #ifdef CONFIG_TOUCHSCREEN_PREVENT_SLEEP
+           if (dt2w_switch > 0 && screen_suspended && prox_covered && code == KEY_POWER)
+           return;
+        #endif
 
 	if (is_event_supported(type, dev->evbit, EV_MAX)) {
 
