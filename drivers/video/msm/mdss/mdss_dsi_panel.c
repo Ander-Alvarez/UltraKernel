5fd368a6ead9c37e6a9d6bd60d1c01057a2b27cd
Eric Farias <ericfari15@gmail.com>
2015-08-31 15:04:08 -0300

    mdss_dsi_panel.c: add powersuspend hooks
    Signed-off-by: Eric Farias <ericfari15@gmail.com>

--- drivers/video/msm/mdss/mdss_dsi_panel.c
+++ drivers/video/msm/mdss/mdss_dsi_panel.c
@@ -770,9 +770,11 @@ static int mdss_dsi_panel_on(struct mdss_panel_data *pdata)
 		pr_err("%s: Invalid input data\n", __func__);
 		return -EINVAL;
 	}
+	
 #ifdef CONFIG_POWERSUSPEND
         set_power_suspend_state_panel_hook(POWER_SUSPEND_INACTIVE);
 #endif
+
 	ctrl = container_of(pdata, struct mdss_dsi_ctrl_pdata,
 				panel_data);
 	mipi  = &pdata->panel_info.mipi;
@@ -932,6 +934,10 @@ disable_regs:
 
 	if (pdata->panel_info.dynamic_cabc_enabled)
 		pdata->panel_info.cabc_mode = CABC_OFF_MODE;
+
+#ifdef CONFIG_POWERSUSPEND
+	set_power_suspend_state_panel_hook(POWER_SUSPEND_ACTIVE);
+#endif
 
 	pr_info("%s-:\n", __func__);
 
