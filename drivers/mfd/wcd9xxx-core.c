0d5052c9c0f4ef61b0e12a82e9b32db126750ee4
srfarias <ericfari15@gmail.com>
2015-09-03 22:58:49 -0300

    Sound Control 3: port to condor

--- drivers/mfd/wcd9xxx-core.c
+++ drivers/mfd/wcd9xxx-core.c
@@ -117,6 +117,22 @@ int wcd9xxx_reg_read(
 	return __wcd9xxx_reg_read(wcd9xxx, reg);
 
 }
+
+#ifdef CONFIG_SOUND_CONTROL_HAX_3_GPL
+int wcd9xxx_reg_read_safe(struct wcd9xxx *wcd9xxx, unsigned short reg)
+{
+ u8 val;
+ int ret;
+
+ ret = wcd9xxx_read(wcd9xxx, reg, 1, &val, false);
+ if (ret < 0)
+ return ret;
+ else
+ return val;
+}
+EXPORT_SYMBOL_GPL(wcd9xxx_reg_read_safe);
+#endif
+
 EXPORT_SYMBOL(wcd9xxx_reg_read);
 
 static int wcd9xxx_write(struct wcd9xxx *wcd9xxx, unsigned short reg,
