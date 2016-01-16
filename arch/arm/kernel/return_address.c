41d1dfa6154b153abb24776bc466c3d0d7a70be4
Ander-Alvarez <pjulian192@gmail.com>
2016-01-16 17:25:21 -0500

    kernel: fix

--- arch/arm/kernel/return_address.c
+++ arch/arm/kernel/return_address.c
@@ -58,11 +58,6 @@ void *return_address(unsigned int level)
 
 #else /* if defined(CONFIG_FRAME_POINTER) && !defined(CONFIG_ARM_UNWIND) */
 
-
-#if 0
-#warning "TODO: return_address should use unwind tables"
-#endif
-
 #if defined(CONFIG_ARM_UNWIND)
 #warning "TODO: return_address should use unwind tables"
 #endif
